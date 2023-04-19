366490f8dad2af2e4a4d295ef68e83e3b6a980a0
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-04-19 17:39:04 +0900

    fix(application): modify entity, dto

--- src/main/java/kr/hellogsm/back_v2/domain/application/dto/request/CreateApplicationReqDto.java
+++ src/main/java/kr/hellogsm/back_v2/domain/application/dto/request/CreateApplicationReqDto.java
@@ -3,6 +3,11 @@ package kr.hellogsm.back_v2.domain.application.dto.request;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import kr.hellogsm.back_v2.domain.application.entity.Application;
+import kr.hellogsm.back_v2.domain.application.entity.admission.AdmissionInfo;
+import kr.hellogsm.back_v2.domain.application.entity.admission.DesiredMajor;
+import kr.hellogsm.back_v2.domain.application.entity.grade.MiddleSchoolGrade;
+import kr.hellogsm.back_v2.domain.application.entity.status.AdmissionStatus;
 import kr.hellogsm.back_v2.domain.application.enums.Gender;
 import kr.hellogsm.back_v2.domain.application.enums.GraduationStatus;
 
@@ -15,6 +20,9 @@ import java.time.LocalDate;
  * @since 1.0.0
  */
 public record CreateApplicationReqDto(
+        @NotBlank
+        String applicantImageUri,
+
         @NotBlank
         String applicantName,
 
@@ -55,9 +63,38 @@ public record CreateApplicationReqDto(
         String teacherPhoneNumber,
 
         @Valid
-        DesiredMajorReqDto DesiredMajorResponseDto,
+        DesiredMajor desiredMajorResponseDto,
 
         @NotBlank
         String MiddleSchoolGrade
 ) {
+    public Application toEntity() {
+        AdmissionInfo admissionInfo = AdmissionInfo.builder()
+                .applicantImageUri(this.applicantImageUri)
+                .applicantName(this.applicantName)
+                .applicantGender(this.applicantGender)
+                .applicantBrith(this.applicantBirth)
+                .address(this.address)
+                .detailAddress(this.detailAddress)
+                .graduation(this.graduation)
+                .telephone(this.telephone)
+                .applicantPhoneNumber(this.applicantPhoneNumber)
+                .guardianName(this.guardianName)
+                .relationWithApplicant(this.relationWithApplicant)
+                .guardianPhoneNumber(this.guardianPhoneNumber)
+                .teacherName(this.teacherName)
+                .teacherPhoneNumber(this.teacherPhoneNumber)
+                .desiredMajor(this.desiredMajorResponseDto)
+                .build();
+
+        AdmissionStatus admissionStatus = AdmissionStatus.init();
+        MiddleSchoolGrade middleSchoolGrade = new MiddleSchoolGrade(null, this.MiddleSchoolGrade);
+
+        return new Application(
+                null,
+                admissionInfo,
+                admissionStatus,
+                middleSchoolGrade
+        );
+    }
 }
