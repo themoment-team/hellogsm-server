76d30d58d4a5a9e07ef7831f573552643d655545
YangSijun528 <yangsijun5528@gmail.com>
2023-04-19 23:32:38 +0900

    fix(temporary): ExistTemporaryServiceImpl에 쓸모없는 @Transactional 제거

--- src/main/java/kr/hellogsm/back_v2/domain/temporary/service/impl/ExistTemporaryServiceImpl.java
+++ src/main/java/kr/hellogsm/back_v2/domain/temporary/service/impl/ExistTemporaryServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(rollbackFor = {Exception.class})
 public class ExistTemporaryServiceImpl implements ExistTemporaryService {
 
     private final TemporaryRepository temporaryRepository;
