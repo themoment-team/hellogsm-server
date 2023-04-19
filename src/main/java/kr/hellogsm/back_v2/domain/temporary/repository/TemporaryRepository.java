c2ac6948bbe56b13dc05b33ea9ef1f1ac308bd9c
YangSijun528 <yangsijun5528@gmail.com>
2023-04-19 23:25:50 +0900

    fix(temporary): 쓸모없는 @Repository 삭제

--- src/main/java/kr/hellogsm/back_v2/domain/temporary/repository/TemporaryRepository.java
+++ src/main/java/kr/hellogsm/back_v2/domain/temporary/repository/TemporaryRepository.java
@@ -2,9 +2,7 @@ package kr.hellogsm.back_v2.domain.temporary.repository;
 
 import kr.hellogsm.back_v2.domain.temporary.entity.Temporary;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TemporaryRepository extends JpaRepository<Temporary, Long> {
     Boolean existsByProviderAndProviderId(String provider, String providerId);
 }
