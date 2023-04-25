5b1f1984894f10f504406d1ac2fc6c1022dabe19
양시준 <80192911+YangSiJun528@users.noreply.github.com>
2023-04-25 20:56:52 +0900

    hotfix(identity): IdentityController 코드블럭 누락 문제 해결

--- src/main/java/kr/hellogsm/back_v2/domain/identity/controller/IdentityController.java
+++ src/main/java/kr/hellogsm/back_v2/domain/identity/controller/IdentityController.java
@@ -56,3 +56,4 @@ public class IdentityController {
         IdentityDto identityResDto = identityQuery.execute(identityId);
         return ResponseEntity.status(HttpStatus.CREATED).body(identityResDto);
     }
+}
