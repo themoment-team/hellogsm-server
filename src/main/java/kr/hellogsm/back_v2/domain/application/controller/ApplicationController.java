3aaceec887fa0b162a9ab3b9eaf020831a58881c
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-04-28 14:55:14 +0900

    feat(application): add response body

--- src/main/java/kr/hellogsm/back_v2/domain/application/controller/ApplicationController.java
+++ src/main/java/kr/hellogsm/back_v2/domain/application/controller/ApplicationController.java
@@ -5,10 +5,14 @@ import jakarta.validation.Valid;
 import kr.hellogsm.back_v2.domain.application.dto.request.CreateApplicationReqDto;
 import kr.hellogsm.back_v2.domain.application.service.CreateApplicationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * 원서를 관리하는 controller 입니다
@@ -23,7 +27,8 @@ public class ApplicationController {
     private final CreateApplicationService createApplicationService;
 
     @PostMapping("/application")
-    public void create(@RequestBody @Valid CreateApplicationReqDto body) throws JsonProcessingException {
+    public ResponseEntity<Map<String, String>> create(@RequestBody @Valid CreateApplicationReqDto body) throws JsonProcessingException {
         createApplicationService.execute(body);
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message", "생성되었습니다"));
     }
 }
