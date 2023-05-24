3f535cc1b1d66a540b3e663dd88f5942730f68e1
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-05-25 08:20:24 +0900

    [#4] 원서 수정 기능 추가 (#42)

--- src/main/java/kr/hellogsm/back_v2/domain/application/controller/ApplicationController.java
+++ src/main/java/kr/hellogsm/back_v2/domain/application/controller/ApplicationController.java
@@ -1,17 +1,15 @@
 package kr.hellogsm.back_v2.domain.application.controller;
 
 import jakarta.validation.Valid;
-import kr.hellogsm.back_v2.domain.application.dto.request.CreateApplicationReqDto;
+import kr.hellogsm.back_v2.domain.application.dto.request.ApplicationReqDto;
 import kr.hellogsm.back_v2.domain.application.service.CreateApplicationService;
+import kr.hellogsm.back_v2.domain.application.service.ModifyApplicationService;
 import kr.hellogsm.back_v2.global.security.oauth.UserInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -26,13 +24,23 @@ import java.util.Map;
 @RequestMapping("/application/v1")
 public class ApplicationController {
     private final CreateApplicationService createApplicationService;
+    private final ModifyApplicationService modifyApplicationService;
 
     @PostMapping("/application")
     public ResponseEntity<Map<String, String>> create(
-            @RequestBody @Valid CreateApplicationReqDto body,
+            @RequestBody @Valid ApplicationReqDto body,
             @AuthenticationPrincipal UserInfo userInfo
     ) {
         createApplicationService.execute(body, userInfo.getUserId());
         return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message", "생성되었습니다"));
+    }
+
+    @PutMapping("/application")
+    public ResponseEntity<Map<String, String>> modify(
+            @RequestBody @Valid ApplicationReqDto body,
+            @AuthenticationPrincipal UserInfo userInfo
+    ) {
+        modifyApplicationService.execute(body, userInfo.getUserId());
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message", "수정되었습니다"));
     }
 }
