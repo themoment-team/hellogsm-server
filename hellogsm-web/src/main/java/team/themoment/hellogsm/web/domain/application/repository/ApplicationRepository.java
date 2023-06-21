44e14308dbc696c395a0bd36b7c31fb147da2c05
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-06-21 15:35:49 +0900

    fix(application): remove not use import

--- hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/application/repository/ApplicationRepository.java
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/application/repository/ApplicationRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import team.themoment.hellogsm.entity.domain.application.entity.Application;
 import team.themoment.hellogsm.entity.domain.application.enums.EvaluationStatus;
 
-import java.util.List;
 import java.util.Optional;
 
 /**
