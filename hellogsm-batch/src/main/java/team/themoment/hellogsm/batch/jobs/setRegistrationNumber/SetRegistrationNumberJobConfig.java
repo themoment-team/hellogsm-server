8e64aa1ae9b1780a06d9bdd5e3a1d37d9cdfc19f
YangSijun528 <yangsijun5528@gmail.com>
2023-06-30 20:20:24 +0900

    chore: 주석 설명 정정

--- hellogsm-batch/src/main/java/team/themoment/hellogsm/batch/jobs/setRegistrationNumber/SetRegistrationNumberJobConfig.java
+++ hellogsm-batch/src/main/java/team/themoment/hellogsm/batch/jobs/setRegistrationNumber/SetRegistrationNumberJobConfig.java
@@ -63,7 +63,7 @@ public class SetRegistrationNumberJobConfig {
                 .start(setRegistrationNumberStep())  // 접수번호 할당 배치 시작
                     .on("FAILED")  // 만약 배치 작업을 실패하면
                     .to(clearRegistrationNumberStep()) // 접수번호 초기화 배치 수행
-                .from(clearRegistrationNumberStep())  // 접수번호 초기화 배치
+                .from(clearRegistrationNumberStep())  // 접수번호 할당 배치
                     .on("*") // 실행 성공 여부와 상관없이
                     .end() // 배치 작업 종료
                 .end()
