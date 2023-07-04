1c1ecae31615dc302a845af8f1236b4e109e1a9c
양시준 <80192911+YangSiJun528@users.noreply.github.com>
2023-07-04 11:54:21 +0900

    사용하지 않는 통합테스트 클래스 비활성화 (#108)

--- hellogsm-batch/src/test/java/team/themoment/hellogsm/batch/HellogsmBatchApplicationTests.java
+++ hellogsm-batch/src/test/java/team/themoment/hellogsm/batch/HellogsmBatchApplicationTests.java
@@ -3,7 +3,7 @@ package team.themoment.hellogsm.batch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class HellogsmBatchApplicationTests {
 
     @Test
