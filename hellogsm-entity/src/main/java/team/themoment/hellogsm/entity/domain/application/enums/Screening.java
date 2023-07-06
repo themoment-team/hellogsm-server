e86af21eea9b72d1cfb0cc7c7691fab398535125
YangSijun528 <yangsijun5528@gmail.com>
2023-07-06 10:08:18 +0900

    feature(applicaiton): Screening 타입 추가 및 관련 DTO, 테스크코드 수정

--- hellogsm-entity/src/main/java/team/themoment/hellogsm/entity/domain/application/enums/Screening.java
+++ hellogsm-entity/src/main/java/team/themoment/hellogsm/entity/domain/application/enums/Screening.java
@@ -1,5 +1,8 @@
 package team.themoment.hellogsm.entity.domain.application.enums;
 
 public enum Screening {
-    GENERAL, SOCIAL, SPECIAL
+    GENERAL, 
+    SOCIAL,
+    SPECIAL_VETERANS, // 국가보훈대상자
+    SPECIAL_ADMISSION, // 특례입학대상자
 }
