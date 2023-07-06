3f64d7651287880aad57a922db53063d4b28170a
YangSijun528 <yangsijun5528@gmail.com>
2023-07-06 10:26:09 +0900

    test: 배치 테스트 코드를 위한 config 클래스 추가

---
+++ hellogsm-batch/src/test/java/team/themoment/hellogsm/batch/TestBatchConfig.java
@@ -0,0 +1,11 @@
+package team.themoment.hellogsm.batch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+@EnableAutoConfiguration
+public class TestBatchConfig {
+}
