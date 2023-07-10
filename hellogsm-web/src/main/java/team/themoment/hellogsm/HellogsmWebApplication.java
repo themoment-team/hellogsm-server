471e74af10fac68497df44853ce29a69aca3e681
양시준 <80192911+YangSiJun528@users.noreply.github.com>
2023-07-10 22:38:49 +0900

    환경변수 설정 변경 (#145)

--- hellogsm-web/src/main/java/team/themoment/hellogsm/HellogsmWebApplication.java
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/HellogsmWebApplication.java
@@ -3,11 +3,10 @@ package team.themoment.hellogsm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import team.themoment.hellogsm.web.global.schedule.ScheduleEnvironment;
 import team.themoment.hellogsm.web.global.security.auth.AuthEnvironment;
 
 @SpringBootApplication
-@EnableConfigurationProperties({AuthEnvironment.class, ScheduleEnvironment.class})
+@EnableConfigurationProperties({AuthEnvironment.class})
 public class HellogsmWebApplication {
 
     public static void main(String[] args) {
