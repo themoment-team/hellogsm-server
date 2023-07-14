97fca5d78a0239715516f8591f7af53008aac522
YangSijun528 <yangsijun5528@gmail.com>
2023-07-14 10:54:49 +0900

    feat: AWS 예외처리 담당하는 객체 구현

---
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/web/global/thirdParty/aws/service/common/AwsCallBack.java
@@ -0,0 +1,12 @@
+package team.themoment.hellogsm.web.global.thirdParty.aws.service.common;
+
+import io.awspring.cloud.s3.S3Exception;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
+import software.amazon.awssdk.core.exception.SdkClientException;
+
+import java.io.IOException;
+
+@FunctionalInterface
+public interface AwsCallBack<T> {
+    T execute() throws SdkClientException, S3Exception, AwsServiceException, IOException;
+}
