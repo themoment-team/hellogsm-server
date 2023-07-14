d1ec38050855e92eabec17f9dd7ec9eebd3e0e08
양시준 <80192911+YangSiJun528@users.noreply.github.com>
2023-07-14 13:37:22 +0900

    AWS SMS 기능 재사용 가능하도록 독립적인 서비스로 분리 (#150)

---
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/web/global/thirdParty/aws/service/aws/impl/SendSmsServiceImpl.java
@@ -0,0 +1,30 @@
+package team.themoment.hellogsm.web.global.thirdParty.aws.service.aws.impl;
+
+import io.awspring.cloud.sns.sms.SmsMessageAttributes;
+import io.awspring.cloud.sns.sms.SmsType;
+import io.awspring.cloud.sns.sms.SnsSmsTemplate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import team.themoment.hellogsm.web.global.thirdParty.aws.service.aws.SendSmsService;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class SendSmsServiceImpl implements SendSmsService {
+    private final static String MAX_PRICE = "1.00"; // 서버에서 이게 필요한가? 어차피 AWS 설정하면 되는데
+    private final static String SENDER_ID = "hello-gsm";
+    private final static String KR_CODE = "+82";
+    private final SnsSmsTemplate smsTemplate;
+
+    @Override
+    public void execute(String phoneNumber, String message) {
+        smsTemplate
+                .send(createPhoneNumber(phoneNumber), message, SmsMessageAttributes.builder()
+                        .smsType(SmsType.TRANSACTIONAL).maxPrice(MAX_PRICE).senderID(SENDER_ID).build());
+    }
+
+    private static String createPhoneNumber(String phoneNumber) {
+        return KR_CODE + phoneNumber;
+    }
+}
