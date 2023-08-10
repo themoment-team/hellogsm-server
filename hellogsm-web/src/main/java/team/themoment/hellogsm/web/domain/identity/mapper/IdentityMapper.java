a4e13a219ecb02cbc35599ec3ca5a9e87666315f
YangSijun528 <yangsijun5528@gmail.com>
2023-08-10 14:06:24 +0900

    feat: CreateIdentityResDto 관련 Mapper Method 추가

--- hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/identity/mapper/IdentityMapper.java
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/identity/mapper/IdentityMapper.java
@@ -5,8 +5,10 @@ import org.mapstruct.factory.Mappers;
 
 import team.themoment.hellogsm.entity.domain.application.enums.Gender;
 import team.themoment.hellogsm.entity.domain.identity.entity.Identity;
+import team.themoment.hellogsm.entity.domain.user.entity.User;
 import team.themoment.hellogsm.web.domain.identity.dto.domain.IdentityDto;
 import team.themoment.hellogsm.web.domain.identity.dto.request.IdentityReqDto;
+import team.themoment.hellogsm.web.domain.identity.dto.response.CreateIdentityResDto;
 
 @Mapper(
         componentModel = "spring",
@@ -26,6 +28,18 @@ public interface IdentityMapper {
             @Mapping(source = "userId", target = "userId")
     })
     IdentityDto identityToIdentityDto(Identity identity);
+
+    @BeanMapping(ignoreUnmappedSourceProperties = {"provider", "providerId"})
+    @Mappings({
+            @Mapping(source = "identity.id", target = "id"),
+            @Mapping(source = "identity.name", target = "name"),
+            @Mapping(source = "identity.phoneNumber", target = "phoneNumber"),
+            @Mapping(source = "identity.birth", target = "birth"),
+            @Mapping(source = "identity.gender", target = "gender"),
+            @Mapping(source = "user.role", target = "userRole"),
+            @Mapping(source = "identity.userId", target = "userId")
+    })
+    CreateIdentityResDto identityToCreateIdentityResDto(Identity identity, User user);
 
     // MapStruct 써서 구현하기 힘들어서 걍 함
     default Identity identityReqDtoToIdentity(IdentityReqDto identityReqDto, Long userId, Long identityId) {
