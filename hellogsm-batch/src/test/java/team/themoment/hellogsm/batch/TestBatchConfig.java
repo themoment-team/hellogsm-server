dd31ec367faa2098b132dfda46d2a8b88c34d777
양시준 <80192911+YangSiJun528@users.noreply.github.com>
2023-08-24 17:43:24 +0900

    접수번호 할당 배치 로직 및 테스트코드 작성  (#170)

--- hellogsm-batch/src/test/java/team/themoment/hellogsm/batch/TestBatchConfig.java
+++ hellogsm-batch/src/test/java/team/themoment/hellogsm/batch/TestBatchConfig.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableBatchProcessing
 @EnableAutoConfiguration
 public class TestBatchConfig {
 }
