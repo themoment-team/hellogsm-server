cbb24ebac1eb92f9ef2dbd189c948fc92fd8d2b1
YangSiJun528 <yangsijun5528@gmail.com>
2023-08-29 18:21:19 +0900

    feat: finalPassedApplicationOrderByScoreIR() 구현 외 기타 메서드 기본 설정

--- hellogsm-batch/src/main/java/team/themoment/hellogsm/batch/jobs/setFinalMajor/SetFinalMajorJobConfig.java
+++ hellogsm-batch/src/main/java/team/themoment/hellogsm/batch/jobs/setFinalMajor/SetFinalMajorJobConfig.java
@@ -3,12 +3,25 @@ package team.themoment.hellogsm.batch.jobs.setFinalMajor;
 import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+import team.themoment.hellogsm.entity.common.util.OptionalUtils;
+import team.themoment.hellogsm.entity.domain.application.entity.Application;
+import team.themoment.hellogsm.entity.domain.application.entity.status.AdmissionStatus;
 
 @Slf4j
 @Configuration
@@ -20,13 +33,77 @@ public class SetFinalMajorJobConfig {
     private final JobRepository jobRepository;
     private final PlatformTransactionManager platformTransactionManager;
     private final EntityManagerFactory entityManagerFactory;
-    private final SetFinalMajorParameter parameter;
 
     @Bean(BEAN_PREFIX + "parameter")
     @JobScope
     public SetFinalMajorParameter parameter(
-            @Value("#{jobParameters[VERSION]}") Long version
+            @Value("#{jobParameters[VERSION]}") Long version,
+            @Value("#{jobParameters[IOT]}") Integer iot,
+            @Value("#{jobParameters[SW]}") Integer sw,
+            @Value("#{jobParameters[AI]}") Integer ai
     ) {
-        return new SetFinalMajorParameter(version);
+        return new SetFinalMajorParameter(version, iot, sw, ai);
     }
+
+    @Bean(JOB_NAME)
+    public Job setFinalMajorJobConfig() {
+        return new JobBuilder(JOB_NAME, this.jobRepository)
+                .start(setFinalMajorStep())
+                .on("*")
+                .end()
+                .end()
+                .build();
+    }
+
+//    @Bean
+//    @JobScope
+//    public Step clearFinalMajorStep() {
+//
+//    }
+
+    @Bean
+    @JobScope
+    public Step setFinalMajorStep() {
+        return new StepBuilder(BEAN_PREFIX+"setFinalMajorStep", this.jobRepository)
+                .<Application, AdmissionStatus>chunk(CHUNK_SIZE, this.platformTransactionManager)
+                .reader(finalPassedApplicationOrderByScoreIR())
+                .writer(finalMajorIW())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public JpaPagingItemReader<Application> finalPassedApplicationOrderByScoreIR() {
+        return new JpaPagingItemReaderBuilder<Application>()
+                .name(BEAN_PREFIX + "finalPassedApplicationIR")
+                .entityManagerFactory(this.entityManagerFactory)
+                .pageSize(CHUNK_SIZE)
+                .queryString(
+                        "SELECT a " +
+                                "FROM Application a " +
+                                "WHERE a.admissionStatus.screeningSecondEvaluationAt IS NOT NULL " +
+                                "AND ( a.admissionStatus.screeningSecondEvaluationAt = GENERAL " +
+                                "OR a.admissionStatus.screeningSecondEvaluationAt = SOCIAL )" +
+                                "ORDER BY a.admissionGrade.totalScore DESC, " +
+                                    "(a.admissionGrade.curricularSubtotalScore + a.admissionGrade.artisticScore) DESC, " +
+                                    "a.admissionGrade.grade3Semester1Score DESC, " +
+                                    "(a.admissionGrade.grade2Semester1Score + a.admissionGrade.grade2Semester2Score) DESC, " +
+                                    "a.admissionGrade.grade2Semester2Score DESC, " +
+                                    "a.admissionGrade.grade2Semester1Score DESC, " +
+                                    "a.admissionGrade.extracurricularSubtotalScore DESC, " +
+                                    "a.admissionInfo.applicantBirth ASC"
+                )
+                .build();
+    }
+    
+    @Bean
+    @StepScope
+    public JpaItemWriter<AdmissionStatus> finalMajorIW() {
+        return new JpaItemWriterBuilder<AdmissionStatus>()
+                .usePersist(false) // update
+                .entityManagerFactory(this.entityManagerFactory)
+                .build();
+    }
+
+
 }
