8aa11a9eb4763847affdcf99defb0e58f399f017
양시준 <80192911+YangSiJun528@users.noreply.github.com>
2023-09-07 17:05:43 +0900

    엔티티 관련 객체 javadoc 추가 (#202)

--- hellogsm-entity/src/main/java/team/themoment/hellogsm/entity/domain/identity/event/UpdateIdentityEvent.java
+++ hellogsm-entity/src/main/java/team/themoment/hellogsm/entity/domain/identity/event/UpdateIdentityEvent.java
@@ -2,4 +2,8 @@ package team.themoment.hellogsm.entity.domain.identity.event;
 
 import team.themoment.hellogsm.entity.domain.identity.entity.Identity;
 
+/**
+ * {@link Identity}를 업데이트하는 이벤트를 나타냅니다. <br/>
+ * {@code Identity}의 변경을 구독자에게 알리기 위해 사용됩니다.
+ */
 public record UpdateIdentityEvent(Identity identity) {}
--- hellogsm-entity/src/main/java/team/themoment/hellogsm/entity/domain/user/event/DeleteUserEvent.java
+++ hellogsm-entity/src/main/java/team/themoment/hellogsm/entity/domain/user/event/DeleteUserEvent.java
@@ -2,5 +2,9 @@ package team.themoment.hellogsm.entity.domain.user.event;
 
 import team.themoment.hellogsm.entity.domain.user.entity.User;
 
+/**
+ * {@link User}를 업데이트하는 이벤트를 나타냅니다. <br/>
+ * {@code User}의 삭제를 구독자에게 알리기 위해 사용됩니다.
+ */
 public record DeleteUserEvent(User user) {
 }
