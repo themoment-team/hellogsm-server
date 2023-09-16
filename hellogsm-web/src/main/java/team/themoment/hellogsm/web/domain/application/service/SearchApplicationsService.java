dfe2769c25a497518bcd0cbfcc72db927c203988
jangwooooo <s22018@gsm.hs.kr>
2023-09-16 19:19:50 +0900

    docs: 최종제출이 완료된 사용자를 검색하는 서비스 javadoc 추가

--- hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/application/service/SearchApplicationsService.java
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/application/service/SearchApplicationsService.java
@@ -3,6 +3,9 @@ package team.themoment.hellogsm.web.domain.application.service;
 import team.themoment.hellogsm.web.domain.application.dto.response.SearchApplicationsResDto;
 import team.themoment.hellogsm.web.domain.application.enums.SearchTag;
 
+/**
+ * 최종제출이 완료된 사용자를 검색하는 service interface 입니다.
+ */
 public interface SearchApplicationsService {
     SearchApplicationsResDto execute(Integer page, Integer size, SearchTag tag, String keyword);
 }
