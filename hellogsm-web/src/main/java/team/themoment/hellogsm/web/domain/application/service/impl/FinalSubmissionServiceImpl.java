27d48caa548dd1c8b914bc82e0ef41f7a003dd27
jangwooooo <s22018@gsm.hs.kr>
2023-09-16 17:41:13 +0900

    docs: 현재 사용자의 원서를 최종제출하는 서비스 javadoc 추가

--- hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/application/service/impl/FinalSubmissionServiceImpl.java
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/application/service/impl/FinalSubmissionServiceImpl.java
@@ -12,12 +12,24 @@ import team.themoment.hellogsm.web.domain.application.repository.ApplicationRepo
 import team.themoment.hellogsm.web.domain.application.service.FinalSubmissionService;
 import team.themoment.hellogsm.web.global.exception.error.ExpectedException;
 
+/**
+ * 현재 사용자의 원서를 최종제출하는 service implementation 입니다.
+ */
 @Service
 @XRayEnabled
 @RequiredArgsConstructor
 public class FinalSubmissionServiceImpl implements FinalSubmissionService {
     private final ApplicationRepository applicationRepository;
 
+    /**
+     * 매개변수로 요청한 유저의 userId값을 받아 userId에 해당하는 원서를 찾습니다. <br>
+     * 찾은 원서의 최종제출 여부를 수정하고 새로운 원서 객체를 만들어 저장합니다.
+     *
+     * @param userId 요청한 유저의 pk값
+     * @throws ExpectedException 발생조건은 아래와 같음 <br>
+     *      1. 원서가 존재하지 않을 경우 <br>
+     *      2. 최종제출이 완료된 원서일 경우 <br>
+     */
     @Override
     public void execute(Long userId) {
         Application application = applicationRepository.findByUserId(userId)
