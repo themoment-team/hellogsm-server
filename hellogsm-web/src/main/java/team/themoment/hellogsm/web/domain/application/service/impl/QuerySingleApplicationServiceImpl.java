d2d7569955744cf7baa41ed75cdbcd524eb9219b
jangwooooo <s22018@gsm.hs.kr>
2023-09-16 19:04:38 +0900

    docs: 사용자의 단일 원서 정보를 가져오는 서비스 javadoc 추가

--- hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/application/service/impl/QuerySingleApplicationServiceImpl.java
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/application/service/impl/QuerySingleApplicationServiceImpl.java
@@ -11,12 +11,24 @@ import team.themoment.hellogsm.web.domain.application.repository.ApplicationRepo
 import team.themoment.hellogsm.web.domain.application.service.QuerySingleApplicationService;
 import team.themoment.hellogsm.web.global.exception.error.ExpectedException;
 
+/**
+ * 사용자의 원서 정보를 가져오는 service implementation 입니다.
+ */
 @Service
 @XRayEnabled
 @RequiredArgsConstructor
 public class QuerySingleApplicationServiceImpl implements QuerySingleApplicationService {
     final private ApplicationRepository applicationRepository;
 
+    /**
+     * 매개변수로 userId를 받아 userId에 해당하는 원서를 찾습니다. <br>
+     * 찾은 원서를 바탕으로 단일 원서 정보를 반환합니다.
+     *
+     * @param userId 사용자의 pk값
+     * @return {@link SingleApplicationRes} 단일 원서 정보
+     * @throws ExpectedException 발생조건은 아래와 같음 <br>
+     *      1. 존재하지 않는 유저일 경우 <br>
+     */
     @Override
     public SingleApplicationRes execute(Long userId) {
         Application application = applicationRepository.findByUserIdEagerFetch(userId)
