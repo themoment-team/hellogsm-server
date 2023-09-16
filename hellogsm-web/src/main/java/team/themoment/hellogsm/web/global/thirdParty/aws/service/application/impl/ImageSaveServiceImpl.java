152b25d2d3c15c79cb7f844f33fac99c31f397bd
jangwooooo <s22018@gsm.hs.kr>
2023-09-16 17:49:35 +0900

    docs: 이미지를 저장해주는 서비스 javadoc 추가

--- hellogsm-web/src/main/java/team/themoment/hellogsm/web/global/thirdParty/aws/service/application/impl/ImageSaveServiceImpl.java
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/web/global/thirdParty/aws/service/application/impl/ImageSaveServiceImpl.java
@@ -11,12 +11,24 @@ import team.themoment.hellogsm.web.global.thirdParty.aws.service.aws.ImageUpload
 
 import java.util.Objects;
 
+/**
+ * 이미지를 저장하는 service implementation 입니다.
+ */
 @Service
 @XRayEnabled
 @RequiredArgsConstructor
 public class ImageSaveServiceImpl implements ImageSaveService {
     private final ImageUploadService imageUploadService;
 
+    /**
+     * 매개변수로 이미지 파일을 받아 저장 후 이미지 url을 반환해 줍니다.
+     *
+     * @param multipartFile 이미지 파일
+     * @return 이미지 url
+     * @throws ExpectedException 발생조건은 아래와 같음 <br>
+     *      1. 파일이 존재하지 않을 경우 <br>
+     *      2. 파일 형식이 올바르지 않을 경우 <br>
+     */
     @Override
     public String execute(MultipartFile multipartFile) {
         if (multipartFile.isEmpty()) throw new ExpectedException("파일이 존재하지 않습니다", HttpStatus.BAD_REQUEST);
