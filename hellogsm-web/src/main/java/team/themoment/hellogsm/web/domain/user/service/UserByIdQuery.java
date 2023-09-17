933dc7e1a6717b469f93110a12664b49f852584c
hajeu <s22072@gsm.hs.kr>
2023-09-17 15:07:06 +0900

    docs: provider로 user를 찾아 DTO로 반환 하는 코드 javadoc 작성

--- hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/user/service/UserByIdQuery.java
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/user/service/UserByIdQuery.java
@@ -3,7 +3,7 @@ package team.themoment.hellogsm.web.domain.user.service;
 import team.themoment.hellogsm.web.domain.user.dto.domain.UserDto;
 
 /**
- * userId로 user를 찾아서 UserDto로 반환하는 interface입니다.
+ * userId로 user를 찾아서 userDto로 반환하는 interface입니다.
  */
 public interface UserByIdQuery {
     UserDto execute(Long userId);
--- hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/user/service/impl/UserByProviderQueryImpl.java
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/user/service/impl/UserByProviderQueryImpl.java
@@ -11,12 +11,24 @@ import team.themoment.hellogsm.web.domain.user.repository.UserRepository;
 import team.themoment.hellogsm.web.domain.user.service.UserByProviderQuery;
 import team.themoment.hellogsm.web.global.exception.error.ExpectedException;
 
+/**
+ * UserByProviderQuery의 구현체입니다.
+ */
 @Service
 @XRayEnabled
 @RequiredArgsConstructor
 public class UserByProviderQueryImpl implements UserByProviderQuery {
     private final UserRepository userRepository;
 
+    /**
+     * provider와 providerId를 인자로 받아서 user를 찾아오고,
+     * 찾아온 user 정보가 담긴 DTO를 반환합니다.
+     *
+     * @param provider 찾아올 user에 대한 provider
+     * @param providerId 찾아올 user에 대한 providerId
+     * @return 찾아온 user 정보가 담긴 DTO
+     * @throws ExpectedException user가 존재하지 않을 경우 발생
+     */
     @Override
     public UserDto execute(String provider, String providerId) {
         User user = userRepository.findByProviderAndProviderId(provider, providerId)
