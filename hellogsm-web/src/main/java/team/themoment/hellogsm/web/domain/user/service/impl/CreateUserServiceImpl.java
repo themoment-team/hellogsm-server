915dbf0ba43347262cecccdfb6005ecf33c8c94d
hajeu <s22072@gsm.hs.kr>
2023-09-17 13:16:13 +0900

    docs: 유저를 생성하는 코드 javadoc 작성

--- hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/user/service/impl/CreateUserServiceImpl.java
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/user/service/impl/CreateUserServiceImpl.java
@@ -13,6 +13,9 @@ import team.themoment.hellogsm.web.domain.user.repository.UserRepository;
 import team.themoment.hellogsm.web.domain.user.service.CreateUserService;
 import team.themoment.hellogsm.web.global.exception.error.ExpectedException;
 
+/**
+ * CreateUserService의 구현체입니다.
+ */
 @Service
 @XRayEnabled
 @RequiredArgsConstructor
@@ -20,6 +23,13 @@ import team.themoment.hellogsm.web.global.exception.error.ExpectedException;
 public class CreateUserServiceImpl implements CreateUserService {
     private final UserRepository userRepository;
 
+    /**
+     * createUserReqDto를 인자로 받아서 user를 생성하고,
+     * 생성한 user 정보를 담은 DTO를 반환합니다.
+     *
+     * @param createUserReqDto 생설할 user 정보가 담긴 DTO
+     * @return 생성된 user 정보를 담은 DTO
+     */
     @Override
     public UserDto execute(CreateUserReqDto createUserReqDto) {
         checkExistUser(createUserReqDto);
@@ -27,6 +37,13 @@ public class CreateUserServiceImpl implements CreateUserService {
         return UserMapper.INSTANCE.userToUserDto(savedTemp);
     }
 
+    /**
+     * createUserReqDto를 인자로 받아서 user 유뮤를 판별하고,
+     * 이미 존재하는 user일 경우 예외를 발생
+     *
+     * @param createUserReqDto 생성할 user 정보가 담긴 DTO
+     * @throws ExpectedException 이미 존재하는 user 일때 발생
+     */
     private void checkExistUser(CreateUserReqDto createUserReqDto) {
         Boolean isExist = userRepository.existsByProviderAndProviderId(
                 createUserReqDto.provider(),
