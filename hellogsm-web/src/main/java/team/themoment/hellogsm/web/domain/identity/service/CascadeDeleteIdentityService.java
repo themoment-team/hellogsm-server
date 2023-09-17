27cbea2e11393a152c9d83d4f43b663f6516153f
hajeu <s22072@gsm.hs.kr>
2023-09-18 01:18:21 +0900

    docs: identity를 삭제하는 코드 javadoc 작성

--- hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/identity/service/CascadeDeleteIdentityService.java
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/identity/service/CascadeDeleteIdentityService.java
@@ -1,5 +1,8 @@
 package team.themoment.hellogsm.web.domain.identity.service;
 
+/**
+ * userid를 받아 Identity를 삭제하는 인터페이스입니다.
+ */
 public interface CascadeDeleteIdentityService {
     void execute(Long userId);
 }
