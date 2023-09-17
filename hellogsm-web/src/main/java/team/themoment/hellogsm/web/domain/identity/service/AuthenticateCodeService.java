e994291605d85c0e4274130eb0f0f86749fe4c90
hajeu <s22072@gsm.hs.kr>
2023-09-17 21:28:54 +0900

    인증코드를 인증하는 서비스 인터페이스 javadoc 작성

--- hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/identity/service/AuthenticateCodeService.java
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/identity/service/AuthenticateCodeService.java
@@ -2,6 +2,9 @@ package team.themoment.hellogsm.web.domain.identity.service;
 
 import team.themoment.hellogsm.web.domain.identity.dto.request.AuthenticateCodeReqDto;
 
+/**
+ * userId와 AuthenticateCodeReqDto를 받아 AuthenticateCode를 인증하는 인터페이스입니다.
+ */
 public interface AuthenticateCodeService {
     void execute(Long userId, AuthenticateCodeReqDto reqDto);
 }
