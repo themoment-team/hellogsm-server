309f3c078b3c2df92117ce4372ce58fda75c22db
hajeu <s22072@gsm.hs.kr>
2023-09-17 14:27:20 +0900

    docs: 유저 유무를 판별하는 코드 javadoc 작성

--- hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/user/service/ExistUserQuery.java
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/user/service/ExistUserQuery.java
@@ -1,5 +1,8 @@
 package team.themoment.hellogsm.web.domain.user.service;
 
+/**
+ * user의 유뮤를 판별하는 interface입니다.
+ */
 public interface ExistUserQuery {
     Boolean execute(String provider, String providerId);
 }
