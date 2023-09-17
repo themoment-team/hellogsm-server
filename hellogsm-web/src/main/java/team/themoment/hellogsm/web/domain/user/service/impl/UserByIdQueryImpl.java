67106140c2aca01c81d8e164935508d5385d5001
hajeu <s22072@gsm.hs.kr>
2023-09-17 14:54:57 +0900

    docs: userId로 user를 찾아 DTO로 반환하는 코드 javadoc 작성

--- hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/user/service/impl/UserByIdQueryImpl.java
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/user/service/impl/UserByIdQueryImpl.java
@@ -11,12 +11,23 @@ import team.themoment.hellogsm.web.domain.user.repository.UserRepository;
 import team.themoment.hellogsm.web.domain.user.service.UserByIdQuery;
 import team.themoment.hellogsm.web.global.exception.error.ExpectedException;
 
+/**
+ * UserByIdQuery의 구현체입니다.
+ */
 @Service
 @XRayEnabled
 @RequiredArgsConstructor
 public class UserByIdQueryImpl implements UserByIdQuery {
     private final UserRepository userRepository;
 
+    /**
+     * userId를 인자로 받아서 user를 찾아오고,
+     * 찾아온 user 정보가 담긴 DTO를 반환합니다.
+     *
+     * @param userId 찾아올 user에 대한 userId
+     * @return 찾아온 user 정보가 담긴 DTO
+     * @throws ExpectedException user가 존재하지 않을 경우 발생
+     */
     @Override
     public UserDto execute(Long userId) {
         User user = userRepository.findById(userId)
