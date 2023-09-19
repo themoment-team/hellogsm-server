00d78bbb093ef03315809f6411cabce91a1de91b
hajeu <s22072@gsm.hs.kr>
2023-09-19 22:41:21 +0900

    test: user 삭제 성공 단위 테스트 코드 작성

---
+++ hellogsm-web/src/test/java/team/themoment/hellogsm/web/domain/user/service/DeleteUserServiceImplTest.java
@@ -0,0 +1,49 @@
+package team.themoment.hellogsm.web.domain.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
+import team.themoment.hellogsm.entity.domain.user.entity.User;
+import team.themoment.hellogsm.entity.domain.user.enums.Role;
+import team.themoment.hellogsm.entity.domain.user.event.DeleteUserEvent;
+import team.themoment.hellogsm.web.domain.user.repository.UserRepository;
+import team.themoment.hellogsm.web.domain.user.service.impl.DeleteUserServiceImpl;
+import team.themoment.hellogsm.web.global.exception.error.ExpectedException;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class DeleteUserServiceImplTest {
+
+    @InjectMocks
+    private DeleteUserServiceImpl deleteUserService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    private final User user = new User(1L,"google", "12345678", Role.ROLE_UNAUTHENTICATED);
+
+    @Test
+    public void 성공(){
+        //given
+        given(userRepository.findById(any(Long.class))).willReturn(Optional.of(user));
+
+        //when
+        deleteUserService.execute(user.getId());
+
+        //then
+        verify(applicationEventPublisher).publishEvent(any(DeleteUserEvent.class));
+    }
+}
