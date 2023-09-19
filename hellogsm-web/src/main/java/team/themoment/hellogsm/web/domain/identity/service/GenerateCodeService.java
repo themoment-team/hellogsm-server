340315ef290b7043d2ed076755b1a2a928f40b71
하제우 <103640011+hajeu@users.noreply.github.com>
2023-09-19 13:52:20 +0900

    Web identity 도메인의 서비스 로직 javadoc 작성 (#225)

--- hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/identity/service/GenerateCodeService.java
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/web/domain/identity/service/GenerateCodeService.java
@@ -1,7 +1,19 @@
 package team.themoment.hellogsm.web.domain.identity.service;
 
+import team.themoment.hellogsm.web.domain.identity.domain.AuthenticationCode;
 import team.themoment.hellogsm.web.domain.identity.dto.request.GenerateCodeReqDto;
 
+/**
+ * code를 생성하는 인터페이스입니다.
+ * @see AuthenticationCode
+ */
 public interface GenerateCodeService {
+    /**
+     * code를 생성합니다.
+     *
+     * @param userId 코드를 생성하기 위한 user의 userId
+     * @param reqDto 코드를 생성하기 위한 정보가 담긴 DTO
+     * @return 생성된 code가 반환됩니다.
+     */
     String execute(Long userId, GenerateCodeReqDto reqDto);
 }
