b05cb5a97266ab9da206cbf74eee3b66d5437086
Junho-06 <k34920032@gmail.com>
2023-09-21 17:29:08 +0900

    add: deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 배포 그룹 이름을 확인합니다. (CodeDeploy 환경 변수를 사용)
+DEPLOYMENT_GROUP_NAME=$(echo $DEPLOYMENT_GROUP_NAME)
+
+# 배포 그룹에 따라 다른 작업을 수행합니다.
+if [ "$DEPLOYMENT_GROUP_NAME" == "web-server" ]; then
+  chmod +x /home/ec2-user/hellogsm-server/scripts/dev-deploy.sh
+  /home/ec2-user/hellogsm-server/scripts/web-dev-deploy.sh
+
+else
+  echo "batch deploy"
+  exit 1
+fi
