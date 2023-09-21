27cd11035728f9107c9f851d856417d8588d9803
박준호 <103944584+Junho-06@users.noreply.github.com>
2023-09-21 19:31:17 +0900

    cicd 파이프라인 구축 (#237)

---
+++ scripts/web-dev-deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+BUILD_JAR=$(ls /home/ec2-user/hellogsm-server/hellogsm-web/build/libs/*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+
+DEPLOY_PATH=/home/ec2-user/dev/web/sictak/
+cp $BUILD_JAR $DEPLOY_PATH
+
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+chmod +x $DEPLOY_JAR
+nohup java -jar $DEPLOY_JAR --spring.profiles.active=dev &
