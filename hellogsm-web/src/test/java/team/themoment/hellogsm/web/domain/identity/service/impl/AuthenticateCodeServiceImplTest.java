8eae12b9ef04a8558629edf4a60525f0de26153c
hajeu <s22072@gsm.hs.kr>
2023-09-24 23:22:57 +0900

    test: 존재하지 않는 AuthenticateCode에 대한 단위 테스트 코드 작성

--- hellogsm-web/src/test/java/team/themoment/hellogsm/web/domain/identity/service/impl/AuthenticateCodeServiceImplTest.java
+++ hellogsm-web/src/test/java/team/themoment/hellogsm/web/domain/identity/service/impl/AuthenticateCodeServiceImplTest.java
@@ -8,9 +8,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import team.themoment.hellogsm.web.domain.identity.domain.AuthenticationCode;
 import team.themoment.hellogsm.web.domain.identity.dto.request.AuthenticateCodeReqDto;
 import team.themoment.hellogsm.web.domain.identity.repository.CodeRepository;
-import team.themoment.hellogsm.web.domain.identity.repository.IdentityRepository;
+import team.themoment.hellogsm.web.global.exception.error.ExpectedException;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -30,13 +31,29 @@ public class AuthenticateCodeServiceImplTest {
             new AuthenticationCode("123456", 1L, true, "010-1234-5678", LocalDateTime.now()),
             new AuthenticationCode("654321", 2L, false, "010-8765-4321", LocalDateTime.now()));
 
-    private final AuthenticateCodeReqDto reqDto = new AuthenticateCodeReqDto(code.get(1).getCode());
+    private final AuthenticateCodeReqDto reqDto = new AuthenticateCodeReqDto(code.get(0).getCode());
 
     @Test
     public void 성공(){
+        //given
         given(codeRepository.findByUserId(any(Long.class))).willReturn(code);
-        given(codeRepository.save(any(AuthenticationCode.class))).willReturn(code.get(1));
+        given(codeRepository.save(any(AuthenticationCode.class))).willReturn(code.get(0));
 
-        assertDoesNotThrow(() -> authenticateCodeService.execute(code.get(1).getUserId(), reqDto));
+        //when & then
+        assertDoesNotThrow(() -> authenticateCodeService.execute(code.get(0).getUserId(), reqDto));
+    }
+
+    @Test
+    public void 존재하지_않는_AuthenticateCode(){
+        //given
+        given(codeRepository.findByUserId(any(Long.class))).willReturn(Collections.emptyList());
+
+        //when & then
+        ExpectedException exception = assertThrows(ExpectedException.class,
+                () -> authenticateCodeService.execute(code.get(0).getUserId(), reqDto));
+
+        String expectedMessage = "사용자의 code가 존재하지 않습니다. 사용자의 ID : " + code.get(0).getUserId();
+
+        assertEquals(exception.getMessage(), expectedMessage);
     }
 }
