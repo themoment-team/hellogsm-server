8d8665e322b645d8ffb467603fbb3ad5c56cd280
hajeu <s22072@gsm.hs.kr>
2023-09-26 11:35:51 +0900

    test: 최신이 아닌 code에 대한 단위 테스트 코드 작성

--- hellogsm-web/src/test/java/team/themoment/hellogsm/web/domain/identity/service/impl/ModifyIdentityServiceImplTest.java
+++ hellogsm-web/src/test/java/team/themoment/hellogsm/web/domain/identity/service/impl/ModifyIdentityServiceImplTest.java
@@ -42,26 +42,27 @@ public class ModifyIdentityServiceImplTest {
     private UserRepository userRepository;
 
     private final List<AuthenticationCode> codes = List.of(
-            new AuthenticationCode("123456", 1L, true, "010-1234-5678", LocalDateTime.MAX),
-            new AuthenticationCode("654321", 1L, true, "010-1234-5678", LocalDateTime.MIN));
+            new AuthenticationCode("123456", 1L, true, "01012345678", LocalDateTime.MIN),
+            new AuthenticationCode("654321", 1L, true, "01012345678", LocalDateTime.MAX));
 
     private final Identity identity = new Identity(1L, "이정우", "01012345678", LocalDate.EPOCH, Gender.MALE, 1L);
 
-    private final IdentityReqDto reqDto = new IdentityReqDto("654321", "이정우", "010-1234-5678", "MALE", LocalDate.EPOCH);
+    private final IdentityReqDto reqDto = new IdentityReqDto("654321", "이정우", "01012345678", "MALE", LocalDate.EPOCH);
 
     private void givenExistingUser(Boolean value){
         given(userRepository.existsById(any(Long.class))).willReturn(value);
     }
 
     private void givenValidIdentity(Boolean identityExists){
-        given(identityRepository.findByUserId(any(Long.class))).willReturn(identityExists ? Optional.of(identity) : Optional.empty());
+        given(identityRepository.findByUserId(any(Long.class)))
+                .willReturn(identityExists ? Optional.of(identity) : Optional.empty());
     }
 
     private void givenValidCode(List<AuthenticationCode> codes){
         given(codeRepository.findByUserId(any(Long.class))).willReturn(codes);
     }
 
-    private void assertThrowsExpectedExceptionWithMessageAndUserId(String expectedMessage, Long userId){
+    private void assertThrowsExpectedExceptionWithMessageAndReqDtoAndUserId(String expectedMessage, IdentityReqDto reqDto, Long userId){
 
         ExpectedException exception = assertThrows(ExpectedException.class,
                 () -> modifyIdentityService.execute(reqDto, userId));;
@@ -82,7 +83,8 @@ public class ModifyIdentityServiceImplTest {
         IdentityDto result = modifyIdentityService.execute(reqDto, identity.getUserId());
 
         //then
-        IdentityDto expectedResult = new IdentityDto(identity.getId(), identity.getName(), identity.getPhoneNumber(), identity.getBirth(), identity.getGender(), identity.getUserId());
+        IdentityDto expectedResult = new IdentityDto(
+                identity.getId(), identity.getName(), identity.getPhoneNumber(), identity.getBirth(), identity.getGender(), identity.getUserId());
 
         assertEquals(result, expectedResult);
     }
@@ -93,7 +95,7 @@ public class ModifyIdentityServiceImplTest {
         givenExistingUser(false);
 
         //when & then
-        assertThrowsExpectedExceptionWithMessageAndUserId("존재하지 않는 User 입니다", identity.getUserId());
+        assertThrowsExpectedExceptionWithMessageAndReqDtoAndUserId("존재하지 않는 User 입니다", reqDto, identity.getUserId());
     }
 
     @Test
@@ -103,20 +105,35 @@ public class ModifyIdentityServiceImplTest {
         givenValidIdentity(false);
 
         //when & then
-        assertThrowsExpectedExceptionWithMessageAndUserId("존재하지 않는 Identity 입니다", identity.getUserId());
+        assertThrowsExpectedExceptionWithMessageAndReqDtoAndUserId("존재하지 않는 Identity 입니다", reqDto, identity.getUserId());
     }
 
     @Test
     public void 인증받지_않은_Code(){
-        final List<AuthenticationCode> unauthenticatedCodes = List.of(
-                new AuthenticationCode("123456", 1L, false, "010-1234-5678", LocalDateTime.MAX),
-                new AuthenticationCode("654321", 1L, false, "010-1234-5678", LocalDateTime.MIN));
-
         //given
+        final List<AuthenticationCode> unauthenticatedCodes = List.of(
+                new AuthenticationCode("123456", 1L, false, "01012345678", LocalDateTime.MIN),
+                new AuthenticationCode("654321", 1L, false, "01012345678", LocalDateTime.MAX));
+
         givenExistingUser(true);
         givenValidIdentity(true);
         givenValidCode(unauthenticatedCodes);
 
-        assertThrowsExpectedExceptionWithMessageAndUserId("유효하지 않은 요청입니다. 인증받지 않은 code입니다.", identity.getUserId());
+        //when & then
+        assertThrowsExpectedExceptionWithMessageAndReqDtoAndUserId("유효하지 않은 요청입니다. 인증받지 않은 code입니다.", reqDto, identity.getUserId());
+    }
+
+    @Test
+    public void 유효하지_않거나_최신이_아닌_Code(){
+        //given
+        final IdentityReqDto 인증코드가_최신이_아닌_IdentityReqDto = new IdentityReqDto(
+                codes.get(0).getCode(), reqDto.name(), reqDto.phoneNumber(), reqDto.gender(), reqDto.birth());
+
+        givenExistingUser(true);
+        givenValidIdentity(true);
+        givenValidCode(codes);
+
+        assertThrowsExpectedExceptionWithMessageAndReqDtoAndUserId(
+                "유효하지 않은 요청입니다. 이전 혹은 잘못된 형식의 code입니다.", 인증코드가_최신이_아닌_IdentityReqDto, identity.getUserId());
     }
 }
