cb617aae86627ceec798184a500963ab20bb24d1
hajeu <s22072@gsm.hs.kr>
2023-09-26 09:01:06 +0900

    test: 존재하지 않는 Identity에 대한 단위 테스트 코드 작성

--- hellogsm-web/src/test/java/team/themoment/hellogsm/web/domain/identity/service/impl/IdentityQueryImplTest.java
+++ hellogsm-web/src/test/java/team/themoment/hellogsm/web/domain/identity/service/impl/IdentityQueryImplTest.java
@@ -9,11 +9,13 @@ import team.themoment.hellogsm.entity.domain.application.enums.Gender;
 import team.themoment.hellogsm.entity.domain.identity.entity.Identity;
 import team.themoment.hellogsm.web.domain.identity.dto.domain.IdentityDto;
 import team.themoment.hellogsm.web.domain.identity.repository.IdentityRepository;
+import team.themoment.hellogsm.web.global.exception.error.ExpectedException;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
@@ -28,11 +30,15 @@ public class IdentityQueryImplTest {
 
     private final Identity identity = new Identity(1L, "아이유", "01012345678", LocalDate.EPOCH, Gender.FEMALE, 1L);
 
+    private void givenValidIdentity(Boolean identityExists){
+        given(identityRepository.findByUserId(any(Long.class)))
+                .willReturn(identityExists ? Optional.of(identity) : Optional.empty());
+    }
 
     @Test
     public void 성공(){
         //given
-        given(identityRepository.findByUserId(any(Long.class))).willReturn(Optional.of(identity));
+        givenValidIdentity(true);
 
         //when
         IdentityDto result = identityQuery.execute(identity.getUserId());
@@ -41,5 +47,19 @@ public class IdentityQueryImplTest {
         IdentityDto expectedResult = new IdentityDto(identity.getId(), identity.getName(), identity.getPhoneNumber(), identity.getBirth(), identity.getGender(), identity.getUserId());
 
         assertEquals(result, expectedResult);
+    }
+
+    @Test
+    public void 존재하지_않는_Identity(){
+        //given
+        givenValidIdentity(false);
+
+        //when & then
+        ExpectedException exception = assertThrows(ExpectedException.class,
+                () -> identityQuery.execute(identity.getUserId()));
+
+        String expectedMessage = "존재하지 않는 Identity 입니다";
+
+        assertEquals(exception.getMessage(), expectedMessage);
     }
 }
